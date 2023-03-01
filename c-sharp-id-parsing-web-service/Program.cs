7f26e555f656333c84e07a0710de0fc8e5a5d67c
IDSCAN\bmulhall <bmulhall@idscan.net>
2023-03-01 14:37:39 -0500

    first commit

---
+++ c-sharp-id-parsing-web-service/Program.cs
@@ -0,0 +1,32 @@
+﻿using RestSharp;
+using System.Text.Json;
+
+namespace idscan{
+    public class Program
+    {
+        static void Main(string[] args) {
+
+            string authKey = "REPLACE_ME";
+
+            var imageFile = File.ReadAllBytes(@"barcode-image.jpg");
+            var data = Convert.ToBase64String(imageFile);
+
+            var client = new RestClient("https://app1.idware.net");
+
+            var request = new RestRequest("/DriverLicenseParserRest.svc/ParseImage", Method.Post);
+
+            // Json to post.
+            //var body = new Body { authKey = authKey, data = data };
+
+            string jsonString = JsonSerializer.Serialize(new { authKey = authKey, data = data });
+
+            request.AddParameter("application/json", jsonString, ParameterType.RequestBody);
+            request.RequestFormat = DataFormat.Json;
+
+            var response = client.Execute(request);
+
+
+        }
+
+    }
+}
